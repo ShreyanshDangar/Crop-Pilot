fba1911a2c1e1b0f9e4cee83183843648d466587
ShreyanshDangar <shreyanshdangar.work@gmail.com>
2026-03-21 23:17:17 +0530

    feat: mount React app at root

---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./store/index.js";
+import App from "./App.jsx"
+import "./styles/base.css";
+import "./styles/components.css";
+import "./styles/animations.css";
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
